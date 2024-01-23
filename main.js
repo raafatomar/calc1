5ad212fce4932c2f85832fe57bb1804cd3cafdff
raafatomar <134744932+raafatomar@users.noreply.github.com>
2024-01-23 15:32:17 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let output = document.getElementById("calc");
+
+function display(num){
+    output.value += num ;
+}
+function Clear(){
+    output.value = "" ;
+}
+function Delete(){
+    output.value = output.value.slice(0, -1);
+}
+function calc(){
+    try{
+        output.value = eval(output.value)
+    }
+    catch(err){
+        output.value = "Error";
+    }
+}
